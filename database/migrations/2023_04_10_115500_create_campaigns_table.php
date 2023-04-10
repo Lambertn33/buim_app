0d5853483618ce43fb1649fe6ca6a060fdf9cb24
Lambertn33 <lambertgithub@gmail.com>
2023-04-10 14:02:08 +0200

    feat: add campaigns model

---
+++ database/migrations/2023_04_10_115500_create_campaigns_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('campaigns', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('manager_id');
+            $table->string('title');
+            $table->enum('province', \App\Models\Campaign::PROVINCES);
+            $table->string('district');
+            $table->string('description');
+            $table->date('from');
+            $table->date('to');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('campaigns');
+    }
+};
