ea599e1d67a263623c530c1b2d472a5a487b032b
Lambertn33 <lambertgithub@gmail.com>
2023-04-14 12:17:46 +0200

    feat: add initialization code and approval to stock device resource

--- app/Filament/Resources/StockDeviceResource/Pages/ManageStockDevices.php
+++ app/Filament/Resources/StockDeviceResource/Pages/ManageStockDevices.php
@@ -3,12 +3,14 @@
 namespace App\Filament\Resources\StockDeviceResource\Pages;
 
 use App\Filament\Resources\StockDeviceResource;
+use App\Models\Role;
 use App\Models\StockDevice;
 use App\Models\StockModel;
 use App\Services\StockServices;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\ManageRecords;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Auth;
 use Filament\Notifications\Notification;
 use Filament\Pages\Actions\Action;
 use Konnco\FilamentImport\Actions\ImportAction;
@@ -20,12 +22,17 @@ class ManageStockDevices extends ManageRecords
 
     protected function getActions(): array
     {
+        
         return [
             Actions\CreateAction::make()
                 ->label('Create device')
                 ->mutateFormDataUsing(function (array $data): array {
+                    $now = now()->format('dmy');
+                    $randomNumber = rand(10000, 99999);
+                    $initializationCode = 'ST-'.$now.'-'.$randomNumber.'';
                     $data['id'] = Str::uuid()->toString();
-
+                    $data['initialization_code'] = $initializationCode;
+                    $data['is_approved'] = true;
                     return $data;
                 })
                 ->successNotification(
@@ -44,17 +51,27 @@ class ManageStockDevices extends ManageRecords
                 ->handleBlankRows(true)
                 ->fields([
                     ImportField::make('device_name')
+                        ->required()
                         ->label('device name'),
                     ImportField::make('serial_number')
+                        ->required()
                         ->label('serial number'),
                     ImportField::make('model')
                         ->mutateBeforeCreate(fn ($value) => StockModel::where('name', 'LIKE', "%{$value}%")->value('id'))
                         ->label('model')
+                        ->required()
                 ])->handleRecordCreation(function ($data) {
+                    $now = now()->format('dmyhis');
                     $data['id'] = Str::uuid()->toString();
                     $data['model_id'] = $data['model'];
+                    $data['initialization_code'] = 'ST-'.$now.'';
                     $data['created_at'] = now();
                     $data['updated_at'] = now();
+                    if (Auth::user()->role->role === Role::ADMIN_ROLE || Auth::user()->role->role === Role::STOCK_MANAGER_ROLE) {
+                        $data['is_approved'] = true;
+                    } else {
+                        $data['is_approved'] = false;
+                    }
                     return StockDevice::create($data);
                 })
         ];
