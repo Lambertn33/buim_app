c3acd2adf8b6bee30a7d1440830b06de76ea4487
Lambertn33 <lambertgithub@gmail.com>
2023-04-26 11:58:05 +0200

    feat: add warehouse device requested devices

---
+++ database/migrations/2023_04_26_094842_create_warehouse_device_requested_devices_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('warehouse_device_requested_devices', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('model_id');
+            $table->uuid('warehouse_device_request_id');
+            $table->string('screener_code');
+            $table->string('device_name');
+            $table->bigInteger('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('warehouse_device_requested_devices');
+    }
+};
