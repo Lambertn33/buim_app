666b65010b34084aaa5d9779443320f07fa1fee0
Lambertn33 <lambertgithub@gmail.com>
2023-04-26 14:49:24 +0200

    feat: district manager only view his/her warehouse device requests

--- app/Filament/Resources/WarehouseDeviceRequestResource/Pages/ListWarehouseDeviceRequests.php
+++ app/Filament/Resources/WarehouseDeviceRequestResource/Pages/ListWarehouseDeviceRequests.php
@@ -3,10 +3,12 @@
 namespace App\Filament\Resources\WarehouseDeviceRequestResource\Pages;
 
 use App\Filament\Resources\WarehouseDeviceRequestResource;
+use App\Models\Role;
 use App\Models\WarehouseDeviceRequest;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\ListRecords;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\Auth;
 
 class ListWarehouseDeviceRequests extends ListRecords
 {
@@ -20,6 +22,12 @@ class ListWarehouseDeviceRequests extends ListRecords
 
     protected function getTableQuery(): Builder
     {
-        return parent::getTableQuery()->whereNot('request_status', WarehouseDeviceRequest::INITIATED);
+        if (Auth::user()->role->role == Role::DISTRICT_MANAGER_ROLE) {
+            return parent::getTableQuery()->whereNot('request_status', WarehouseDeviceRequest::INITIATED)->whereHas('campaign', function($query){
+                $query->where('manager_id', Auth::user()->manager->id);
+            });
+        } else {
+            return parent::getTableQuery()->whereNot('request_status', WarehouseDeviceRequest::INITIATED);
+        }
     }
 }
